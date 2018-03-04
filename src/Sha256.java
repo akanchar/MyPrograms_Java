e8fbba3116aa3899b4a5b8703c50738a776b1b67
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-04 17:58:34 -0600

    ADD: Base SHA 256 java file

---
+++ src/Sha256.java
@@ -0,0 +1,33 @@
+public class Sha256
+{
+    static final int sha_algo_number = 256;
+    static final int initialPrimes = 8;
+    static final int secondaryPrimes = 64;
+    public static void main(String[] args)
+    {
+
+    }
+    static String[] fractionalPrimes(int numberOfPrimes, String root)
+    {
+        String[] primeInBits = new String[numberOfPrimes];
+        switch (root)
+        {
+            case "square":
+                int[] firstPrimes = return_N_Primes(numberOfPrimes);
+                break;
+            case "cube":
+                break;
+        }
+        return null;
+    }
+
+    private static int[] return_N_Primes(int numberOfPrimes) {
+        int count = 0, number = 2;
+        int[] firstPrimes = new int[numberOfPrimes];
+        while(count < numberOfPrimes)
+        {
+
+        }
+        return firstPrimes;
+    }
+}
