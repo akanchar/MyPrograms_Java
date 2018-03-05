c6c2cbe222fcd90b364d90633eadb8c6797e5571
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-04 19:45:54 -0600

    ADD: BinaryConversion
    Added empty binary conversion java file.

---
+++ src/BinaryConversion.java
@@ -0,0 +1,15 @@
+/**
+ * <h1>Binary Conversion</h1>
+ * <p>
+ * This class has functions that return the binary converted string of
+ * a number and it fractionalPart.
+ * </p>
+ *
+ * @author Abhilash Kancharla
+ * @since 04-03-2018
+ */
+
+public class BinaryConversion
+{
+  
+}
