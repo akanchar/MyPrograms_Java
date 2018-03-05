f9140299126912f39f41e96a32183a05366d1bb4
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-04 19:46:46 -0600

    MOD: Minor commenting changes
    Minor commenting changes

--- src/Sha256.java
+++ src/Sha256.java
@@ -1,16 +1,14 @@
 import static java.lang.System.out;
 /**
- * <h1>Produces SHA 256 output for a given String</h1>
- * The AddNum program implements an application that
- * simply adds two given integer numbers and Prints
- * the output on the screen.
+ * <h1> SHA 256 </h1>
  * <p>
- * <b>Note:</b> Giving proper comments in your program makes it more
- * user friendly and it is assumed as a high quality code.
+ *   Produces SHA 256 for a given string
+ * </p>
+ *
  * @author Abhilash Kancharla
  * @since 04-03-2018
  */
-
+// Incomplete
 public class Sha256
 {
     static final int sha_algo_number = 256;
@@ -20,12 +18,7 @@ public class Sha256
     public static void main(String[] args)
     {
       PrimeNumber primeNumber = new PrimeNumber();
-      for (int i: primeNumber.first_N_Primes(10))
+      for (int i: primeNumber.rangedPrimes(2,110))
         out.println(i);
-    }
-    private static String[] fractionalPrimes(int numberOfPrimes, String root)
-    {
-        String[] primeInBits = new String[numberOfPrimes];
-        return null;
     }
 }
